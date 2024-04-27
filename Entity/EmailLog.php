20fcc0a8bffa206dc133af1e7c68666caacb015c
TickTackk <me@ticktackk.io>
2024-04-27 12:51:02 +0530

    Fix: `\XF\Mail\Mailer::send()` signature does not match that of XF 2.3 (#131)

--- Entity/EmailLog.php
+++ Entity/EmailLog.php
@@ -6,11 +6,13 @@ use XF\Mvc\Entity\Entity;
 use XF\Mvc\Entity\Structure as EntityStructure;
 
 /**
+ * @version 1.5.0
+ *
  * COLUMNS
  * @property int|null email_id
  * @property string subject
  * @property int log_date
- * @property string return_path
+ * @property array return_path
  * @property array|null sender
  * @property array|null from
  * @property array|null reply_to
@@ -31,7 +33,7 @@ class EmailLog extends Entity
             'email_id' => ['type' => static::UINT, 'autoIncrement' => true, 'nullable' => true],
             'subject' => ['type' => static::STR, 'required' => true],
             'log_date' => ['type' => static::UINT, 'required' => true],
-            'return_path' => ['type' => static::STR, 'required' => true],
+            'return_path' => ['type' => static::JSON, 'required' => true],
             'sender' => ['type' => static::JSON, 'default' => null, 'nullable' => true],
             'from' => ['type' => static::JSON, 'required' => true],
             'reply_to' => ['type' => static::JSON, 'default' => null, 'nullable' => true],
--- XF/Mail/Mailer.php
+++ XF/Mail/Mailer.php
@@ -2,14 +2,19 @@
 
 namespace TickTackk\DeveloperTools\XF\Mail;
 
-$destClass = 'TickTackk\DeveloperTools\XF\Mail\Mailer';
-$srcClass = \XF::$versionId < 2020000
+class_alias(\XF::$versionId < 2020000
     ? 'TickTackk\DeveloperTools\XF\Mail\XF2\Mailer'
-    : 'TickTackk\DeveloperTools\XF\Mail\XF22\Mailer';
+    : (\XF::$versionId >= 2030000
+        ? 'TickTackk\DeveloperTools\XF\Mail\XF23\Mailer'
+        : 'TickTackk\DeveloperTools\XF\Mail\XF22\Mailer'),
+    'TickTackk\DeveloperTools\XF\Mail\Mailer');
 
-class_alias($srcClass, $destClass);
 class_alias(
     'TickTackk\DeveloperTools\XF\Mail\XFCP_Mailer',
-    \XF::$versionId < 2020000 ? 'TickTackk\DeveloperTools\XF\Mail\XF22\XFCP_Mailer' : 'TickTackk\DeveloperTools\XF\Mail\XF2\XFCP_Mailer',
+    \XF::$versionId >= 2020000
+        ? (\XF::$versionId >= 2030000
+        ? 'TickTackk\DeveloperTools\XF\Mail\XF23\XFCP_Mailer'
+        : 'TickTackk\DeveloperTools\XF\Mail\XF22\XFCP_Mailer'
+    ) : 'TickTackk\DeveloperTools\XF\Mail\XF2\XFCP_Mailer',
     false
 );
